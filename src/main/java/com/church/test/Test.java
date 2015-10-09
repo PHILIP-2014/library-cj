4de1f5db9d3fa098381d210068d98b4a9a35769a
wangfl <wangfl@parox.cn>
2015-10-09 09:32:52 +0800

    test目录添加

---
+++ src/main/java/com/church/test/Test.java
@@ -0,0 +1,11 @@
+package com.church.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Integer integer = null;
+		if(integer < 10){
+			System.out.println(integer);
+		}
+	}
+}
